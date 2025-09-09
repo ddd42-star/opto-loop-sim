985b57fa32bc90a7bfe6e564770bc36967d041fe
ddd42-star <dario.bassi111@gmail.com>
2025-09-09 19:19:33 +0200

    simplified state device class. I will not be able to introduce also the UniMMCore with napari micromanager because the widget requires the use of CMMCorePlus and not the experimental core

--- pymmcore_state_device_sim.py
+++ pymmcore_state_device_sim.py
@@ -1,18 +1,10 @@
 from pymmcore_plus.experimental.unicore import StateDevice
 
 
-class SimFilterWheelDevice(StateDevice):
+class SimStateDevice(StateDevice):
 
-    def __init__(self):
-        # add STATES for uv, blue, green, red, farred)
-        state_labels = {
-            0: "RED",
-            1: "BLUE", # excitation 405 nm, emission filter 450/500 nm
-            2: "GREEN", # excitation 488 nm, emission filter 525 nm
-            3: "UV",
-            4: "FARRED"
-        }
-        super().__init__(state_labels)
+    def __init__(self, state_dict: dict[int, str]):
+        super().__init__(state_dict)
         self._current_state = 0 # default position
 
     def get_state(self) -> int:
@@ -25,34 +17,4 @@ class SimFilterWheelDevice(StateDevice):
         """
         Set the current state of the filter wheel
         """
-        if position not in self._state_to_label.keys():
-            raise ValueError("Invalid position")
-        self._current_state = position
-
-
-class SimLEDDevice(StateDevice):
-
-    def __init__(self):
-        state_labels = {
-            0: "RED",
-            1: "BLUE",
-            2: "GREEN",
-            3: "UV",
-            4: "FARED"
-        }
-        super().__init__(state_labels)
-        self._current_state = 0
-
-    def get_state(self) -> int:
-        """
-        Return the current state of the LED Channel
-        """
-        return self._current_state
-
-    def set_state(self, position: int) -> None:
-        """
-        Set the current state of the LED Channel
-        """
-        if position not in self._state_to_label.keys():
-            raise ValueError("Invalid position")
         self._current_state = position
